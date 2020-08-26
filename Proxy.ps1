9712ec01d7d76962155c2aeb26e7b4aeb08c43bf
skadevare <70136435+skadevare@users.noreply.github.com>
2020-08-26 10:35:11 +0200

    Update Proxy.ps1

--- Proxy.ps1
+++ Proxy.ps1
@@ -4,9 +4,14 @@
 # A simple CA installer to trusted roots and proxy changer
 #  
 # ---------------------------------------------------------
+
+# Download CA from b64 encoded blob
 wget https://path.to.your-ca-cert/cert.b64 -o cert.b64
+# Decode certificate
 certutil -decode ".\cert.b64" ".\cert.cer"
+# Import Ceritificate to Machine Root store
 Import-Certificate -FilePath ".\cert.cer" -CertStoreLocation Cert:\LocalMachine\Root
+# Set proxy settings on system
 netsh winhttp set proxy x.x.x.x:8080
 
 
