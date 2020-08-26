e31b8c8c0933ae94014f50c0f0b58c2c8d590106
skadevare <70136435+skadevare@users.noreply.github.com>
2020-08-26 10:31:38 +0200

    Add files via upload

---
+++ antivirus-checker.ps1
@@ -0,0 +1,21 @@
+# ---------------------------------------------------------
+#
+# Skadevare.no
+# A simple antivirus checker with potenial for commands
+# based on result
+#  
+# ---------------------------------------------------------
+$wmiQuery = "SELECT * FROM AntiVirusProduct"
+$AntivirusProduct = Get-WmiObject -Namespace "root\SecurityCenter2" -Query $wmiQuery  @psboundparameters # -ErrorVariable myError -ErrorAction 'SilentlyContinue'             
+            Write-host $AntivirusProduct.displayName
+
+if ($AntivirusProduct.displayName -eq 'Windows Defender')
+    {
+	echo "Installed antivirus is WindowsDefender"
+
+	}
+if ($AntivirusProduct.displayName -eq 'Trend Micro Officescan')
+    {
+	echo "Installed antivirus is  Trend"
+
+	}
